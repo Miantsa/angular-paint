65793706ea7964cc30f549bdcd333776cbe779cf
Miantsa <miantsamiantsarazakamanantsoa@gmail.com>
2021-02-08 11:31:47 +0300

    set canvas resizable and add footer

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,6 +10,59 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.allAboutResizing();
 
+  }
+  allAboutResizing() {
+    // Query the element
+    const ele = document.getElementById('resizeMe');
+
+    // The current position of mouse
+    let x = 0;
+    let y = 0;
+
+    // The dimension of the element
+    let w = 0;
+    let h = 0;
+
+    // Handle the mousedown event
+    // that's triggered when user drags the resizer
+    const mouseDownHandler = function (e) {
+      // Get the current mouse position
+      x = e.clientX;
+      y = e.clientY;
+
+      // Calculate the dimension of element
+      const styles = window.getComputedStyle(ele);
+      w = parseInt(styles.width, 10);
+      h = parseInt(styles.height, 10);
+
+      // Attach the listeners to `document`
+      document.addEventListener('mousemove', mouseMoveHandler);
+      document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function (e) {
+      // How far the mouse has been moved
+      const dx = e.clientX - x;
+      const dy = e.clientY - y;
+
+      // Adjust the dimension of element
+      ele.style.width = `${w + dx}px`;
+      ele.style.height = `${h + dy}px`;
+    };
+
+    const mouseUpHandler = function () {
+      // Remove the handlers of `mousemove` and `mouseup`
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+    };
+    // Query all resizers
+    const resizers = ele.querySelectorAll('.resizer');
+
+    // Loop over them
+    [].forEach.call(resizers, function (resizer) {
+      resizer.addEventListener('mousedown', mouseDownHandler);
+    });
+  }
 }
